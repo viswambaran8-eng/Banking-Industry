7aaff6709e920f8bd94e6c944451b07e7b6e1b5e
sreejith <viswambaran8@gmail.com>
2025-12-02 01:32:48 +0530

    Banking Solutions

---
+++ footer.js
@@ -0,0 +1,19 @@
+VANTA.BIRDS({
+  el: "#vanta-fr",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+
+  backgroundColor: 0xffffff,     // white background
+  color1: 0xE6633C,              // orange shade
+  color2: 0xF21511,              // red shade
+
+  birdSize: 1.20,                // smaller birds
+  wingSpan: 15.00,               // reduced wing span
+  speedLimit: 8.00,
+  separation: 20.00              // slightly tighter grouping
+})
